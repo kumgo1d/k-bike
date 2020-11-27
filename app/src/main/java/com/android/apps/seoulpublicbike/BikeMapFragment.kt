6917681276bff0642b55fbd19f05af857524b5d6
Junghun <junghunism@gmail.com>
2020-11-27 17:09:25 +0900

    location

--- app/src/main/java/com/android/apps/seoulpublicbike/BikeMapFragment.kt
+++ app/src/main/java/com/android/apps/seoulpublicbike/BikeMapFragment.kt
@@ -24,7 +24,9 @@ import com.android.apps.seoulpublicbike.data.Bike
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.*
+import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.Overlay
 import com.naver.maps.map.util.FusedLocationSource
 import retrofit2.Call
 import retrofit2.Callback
@@ -37,6 +39,10 @@ class BikeMapFragment : Fragment(), OnMapReadyCallback, LocationListener {
     private lateinit var naverMap: NaverMap
     private var locationManager: LocationManager? = null
 
+    //내 위치
+    private var longitude: Double = 0.0
+    private var latitude: Double = 0.0
+
     companion object {
         const val LOCATION_PERMISSION_REQUEST_CODE = 100
     }
@@ -44,13 +50,16 @@ class BikeMapFragment : Fragment(), OnMapReadyCallback, LocationListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //map fragment와 sync
         val mapFragment = fragmentManager?.findFragmentById(R.id.map) as MapFragment?
             ?: MapFragment.newInstance().also {
                 fragmentManager?.beginTransaction()?.add(R.id.map, it)?.commit()
             }
         mapFragment.getMapAsync(this)
 
+        //권한 설정 여부와 지도 위 위치 표시를 위한 location source
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        //내 위치를 알기 위한 location manager
         locationManager = (activity as AppCompatActivity).getSystemService(LOCATION_SERVICE) as LocationManager?
         checkPermissions()
     }
@@ -69,6 +78,7 @@ class BikeMapFragment : Fragment(), OnMapReadyCallback, LocationListener {
     override fun onStart() {
         super.onStart()
 
+        //권한
         if (ContextCompat.checkSelfPermission(context!!, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10f, this)
         } else {
@@ -81,19 +91,19 @@ class BikeMapFragment : Fragment(), OnMapReadyCallback, LocationListener {
         locationManager?.removeUpdates(this)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bike_map, container, false)
     }
 
     @UiThread
     override fun onMapReady(naverMap: NaverMap) {
+        var cameraPosition = CameraPosition(LatLng(latitude, longitude), 16.0)
         this.naverMap = naverMap
         naverMap.locationSource = locationSource
         naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        naverMap.cameraPosition = cameraPosition
         naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BICYCLE, true)
-        loadBikeList()
     }
 
     private fun checkPermissions() {
@@ -107,12 +117,34 @@ class BikeMapFragment : Fragment(), OnMapReadyCallback, LocationListener {
     }
 
     private fun loadBikeList() {
+        //Retrofit 사용
         val retrofit = Retrofit.Builder()
             .baseUrl(SeoulOpenApi.DOMAIN)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         val seoulOpenService = retrofit.create(SeoulOpenService::class.java)
-        seoulOpenService.getBike(SeoulOpenApi.API_KEY).enqueue(object : Callback<Bike> {
+        //1 ~ 1000
+        seoulOpenService.getBike(SeoulOpenApi.API_KEY1).enqueue(object : Callback<Bike> {
+            override fun onResponse(call: Call<Bike>, response: Response<Bike>) {
+                showBikeList(response.body() as Bike)
+            }
+
+            override fun onFailure(call: Call<Bike>, t: Throwable) {
+                Toast.makeText(context, "서버 오류가 발생하였습니다.", Toast.LENGTH_LONG).show()
+            }
+        })
+        //1001 ~ 2000
+        seoulOpenService.getBike(SeoulOpenApi.API_KEY2).enqueue(object : Callback<Bike> {
+            override fun onResponse(call: Call<Bike>, response: Response<Bike>) {
+                showBikeList(response.body() as Bike)
+            }
+
+            override fun onFailure(call: Call<Bike>, t: Throwable) {
+                Toast.makeText(context, "서버 오류가 발생하였습니다.", Toast.LENGTH_LONG).show()
+            }
+        })
+        //2001 ~ 3000
+        seoulOpenService.getBike(SeoulOpenApi.API_KEY3).enqueue(object : Callback<Bike> {
             override fun onResponse(call: Call<Bike>, response: Response<Bike>) {
                 showBikeList(response.body() as Bike)
             }
@@ -124,21 +156,51 @@ class BikeMapFragment : Fragment(), OnMapReadyCallback, LocationListener {
     }
 
     fun showBikeList(bike: Bike) {
+        var distance = 0.01f
+        //현재 위치와 자전거 대여소의 위치를 비교하여 내 위치 주변 대여소만 보여준다.
         for(b in bike.rentBikeStatus.row) {
-            val pos = LatLng(b.stationLatitude.toDouble(), b.stationLongitude.toDouble())
-            val marker = Marker()
-            marker.apply {
-                position = pos
-                captionText = b.stationName
-                map = naverMap
+            if(latitude-distance <= b.stationLatitude.toDouble() && b.stationLatitude.toDouble() <= latitude+distance
+                && longitude-distance <= b.stationLongitude.toDouble() && b.stationLongitude.toDouble() <= longitude+distance) {
+                val pos = LatLng(b.stationLatitude.toDouble(), b.stationLongitude.toDouble())
+
+                val infoWindow = InfoWindow()
+                infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(context!!) {
+                    override fun getText(infoWindow: InfoWindow): CharSequence {
+                        return "자전거 : ${b.parkingBikeTotCnt} \n주차가능 : ${b.rackTotCnt}"
+                    }
+                }
+
+                val listener = Overlay.OnClickListener { overlay ->
+                    val marker = overlay as Marker
+
+                    if (marker.infoWindow == null) {
+                        // 현재 마커에 정보 창이 열려있지 않을 경우 엶
+                        infoWindow.open(marker)
+                    } else {
+                        // 이미 현재 마커에 정보 창이 열려있을 경우 닫음
+                        infoWindow.close()
+                    }
+
+                    true
+                }
+
+                val marker = Marker()
+                marker.apply {
+                    width = 80
+                    height = 100
+                    position = pos
+                    map = naverMap
+                    onClickListener = listener
+                }
             }
         }
     }
 
     override fun onLocationChanged(location: Location?) {
-        //Bike 위도,경도와 location 위도,경도 비교
-        //showBikeList()
-        Toast.makeText(context, "dddd", Toast.LENGTH_LONG).show()
+        //현재 위치를 저장하는 변수에 값을 할당한다.
+        longitude = location!!.longitude
+        latitude = location!!.latitude
+        loadBikeList()
     }
 
     override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
--- app/src/main/java/com/android/apps/seoulpublicbike/SeoulOpenApi.kt
+++ app/src/main/java/com/android/apps/seoulpublicbike/SeoulOpenApi.kt
@@ -8,11 +8,13 @@ import retrofit2.http.Path
 class SeoulOpenApi {
     companion object {
         const val DOMAIN = "http://openapi.seoul.go.kr:8088/"
-        const val API_KEY = "6e4e5458526a756e36304c6f78427a"
+        const val API_KEY1 = "6e4e5458526a756e36304c6f78427a/json/bikeList/1/1000"
+        const val API_KEY2 = "6e4e5458526a756e36304c6f78427a/json/bikeList/1001/2000"
+        const val API_KEY3 = "6e4e5458526a756e36304c6f78427a/json/bikeList/2001/3000"
     }
 }
 
 interface SeoulOpenService {
-    @GET("{api_key}/json/bikeList/1/200")
+    @GET("{api_key}")
     fun getBike(@Path("api_key") key: String): Call<Bike>
 }
