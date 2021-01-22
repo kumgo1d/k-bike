8bf0d4b0b28023e818b7c998fb35aa7e9cf0f072
Junghun <junghunism@gmail.com>
2021-01-23 00:41:37 +0900

    add navigation controller and refactoring
    Jetpack Navigation Controller를 추가하였습니다.
    불필요한 Fragment를 삭제하였습니다.
    
    특이사항 : navigate 작동 크래쉬 에러

--- app/src/main/java/com/android/apps/seoulpublicbike/home/HomeFragment.kt
+++ app/src/main/java/com/android/apps/seoulpublicbike/home/HomeFragment.kt
@@ -5,40 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.android.apps.seoulpublicbike.R
-import com.android.apps.seoulpublicbike.base.BaseFragment
 import com.android.apps.seoulpublicbike.databinding.FragmentHomeBinding
+import com.android.apps.seoulpublicbike.favoritelist.FavoriteListFragment
+import com.android.apps.seoulpublicbike.seoul.SeoulBikeMapFragment
 
 class HomeFragment : Fragment() {
 
-    private lateinit var binding: FragmentHomeBinding
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private val seoulBikeMapFragment = SeoulBikeMapFragment()
+    private val favoriteListFragment = FavoriteListFragment()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-
-        navSeoulFragment()
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        navigationFragments()
     }
 
-//    private fun addListener() {
-//        binding.seoulButton.setOnClickListener {
-//            navSeoulFragment()
-//        }
-//    }
-//
-    private fun navSeoulFragment() {
-        val f = BaseFragment()
-        val transaction = childFragmentManager.beginTransaction()
-        transaction.attach(f).commit()
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun navigationFragments() {
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.map_view -> {
+                    findNavController().navigate(R.id.action_homeFragment_to_seoulBikeMapFragment)
+                }
+                R.id.favorite_list -> {
+                    findNavController().navigate(R.id.action_homeFragment_to_favoriteListFragment)
+                }
+            }
+            true
+        }
     }
 }
