6f4134a6bf3481449b5541a3c12e328edb714f5b
JunghunKum <39320407+kumgo1d@users.noreply.github.com>
2021-11-22 21:56:32 +0900

    테스트 코드 작성중
     - 테스트 코드 작성 시도중

--- app/src/androidTest/java/com/goldcompany/apps/koreabike/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/goldcompany/apps/koreabike/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.android.apps.seoulpublicbike", appContext.packageName)
+        assertEquals("com.goldcompany.apps.koreabike", appContext.packageName)
     }
 }
