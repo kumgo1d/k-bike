60f1e1face46a8fe79127fdaa99a9d1eb7400639
JunghunKum <39320407+kumgo1d@users.noreply.github.com>
2022-06-01 18:45:02 +0900

    refactor : refactor viewModel class
    refactor all viewModel annotation
    writing test code

--- app/src/test/java/com/goldcompany/apps/koreabike/search_address/SearchAddressViewModelTest.kt
+++ app/src/test/java/com/goldcompany/apps/koreabike/search_address/SearchAddressViewModelTest.kt
@@ -6,18 +6,21 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.goldcompany.apps.koreabike.MainCoroutineRule
 import com.goldcompany.apps.koreabike.data.KBikeRepository
 import com.goldcompany.apps.koreabike.ui.search_address.SearchAddressViewModel
+import com.goldcompany.apps.koreabike.util.Result
 import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
 import org.robolectric.annotation.Config
 
+//robolectric 에러 방지를 위해 SDK 버전 명시
 @Config(sdk = [Build.VERSION_CODES.O])
+//코루틴 테스트를 위해 명시
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SearchAddressViewModelTest {
@@ -49,6 +52,12 @@ class SearchAddressViewModelTest {
 
     @Test
     fun searchAddress_pagingDataLoaded() {
-        viewModel.searchAddress("현대백화점")
+        mainCoroutineRule.runBlockingTest {
+            val result = Result.Success(
+                viewModel.searchAddress("현대백화점")
+            )
+
+            Assert.assertEquals("", result)
+        }
     }
 }
