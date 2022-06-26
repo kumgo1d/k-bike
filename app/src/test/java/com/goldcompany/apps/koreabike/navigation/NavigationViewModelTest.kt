86281c15c115081a8ee9039a3adcb5588e7a9b11
JunghunKum <39320407+kumgo1d@users.noreply.github.com>
2022-06-26 23:36:56 +0900

    Update NavigationViewModelTest.kt

--- app/src/test/java/com/goldcompany/apps/koreabike/navigation/NavigationViewModelTest.kt
+++ app/src/test/java/com/goldcompany/apps/koreabike/navigation/NavigationViewModelTest.kt
@@ -3,20 +3,13 @@ package com.goldcompany.apps.koreabike.navigation
 
 import android.os.Build
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.goldcompany.apps.koreabike.MainCoroutineRule
-import com.goldcompany.apps.koreabike.api.KakaoApiService
-import com.goldcompany.apps.koreabike.api.NaverApiService
-import com.goldcompany.apps.koreabike.data.KBikeLocalDataSource
-import com.goldcompany.apps.koreabike.data.KBikeRemoteDataSource
 import com.goldcompany.apps.koreabike.data.KBikeRepository
-import com.goldcompany.apps.koreabike.db.KBikeDatabase
-import com.goldcompany.apps.koreabike.di.NetworkModule
 import com.goldcompany.apps.koreabike.ui.navigation.NavigationViewModel
 import io.mockk.MockKAnnotations
-import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runBlockingTest
@@ -27,7 +20,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
-import javax.inject.Inject
 
 @Config(sdk = [Build.VERSION_CODES.O])
 @ExperimentalCoroutinesApi
@@ -49,12 +41,8 @@ class NavigationViewModelTest {
     @Before
     fun setup() {
         MockKAnnotations.init(this, relaxed = true)
+        repository = mockk()
         viewModel = NavigationViewModel(repository)
-    }
-
-    @Test
-    fun testMethod() {
-        Assert.assertEquals(10, 10)
     }
 
     @Test
@@ -62,6 +50,8 @@ class NavigationViewModelTest {
         val start = "126.976861018866,37.5759689663327"
         val end = "127.011795743342,37.2869569586225"
         val message = "길찾기를 성공하였습니다."
+
+        every { runBlockingTest { repository.getNavigationPath(start, end) } } returns mockk()
 
         val result = viewModel.getNavigationPath(start, end).first()
         Assert.assertEquals(message, result.message)
