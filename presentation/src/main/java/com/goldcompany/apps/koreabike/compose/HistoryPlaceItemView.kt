a0649ac893dcee7d2785f6d4aa37591141257b40
JunghunKum <39320407+kumgo1d@users.noreply.github.com>
2022-08-28 22:52:30 +0900

    Update HistoryPlaceItemView.kt

--- presentation/src/main/java/com/goldcompany/apps/koreabike/compose/HistoryPlaceItemView.kt
+++ presentation/src/main/java/com/goldcompany/apps/koreabike/compose/HistoryPlaceItemView.kt
@@ -1,11 +1,13 @@
 package com.goldcompany.apps.koreabike.compose
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -19,11 +21,14 @@ fun HistoryPlaceItemView(
     deleteAddress: () -> Unit,
     onClick: () -> Unit
 ) {
-    Surface(onClick = onClick) {
-        Row(modifier = Modifier.fillMaxWidth()) {
-            val searchIcon = painterResource(id = R.drawable.ic_search_button)
+    Surface(
+        modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        onClick = onClick
+    ) {
+        Row {
             Image(
-                painter = searchIcon,
+                painter = painterResource(id = R.drawable.ic_search_button),
                 contentDescription = null,
                 modifier = Modifier.padding(horizontal = 8.dp)
             )
@@ -35,14 +40,13 @@ fun HistoryPlaceItemView(
                     text = address.addressName
                 )
             }
-            Surface(onClick = deleteAddress) {
-                val deleteIcon = painterResource(id = R.drawable.ic_delete_button)
-                Image(
-                    painter = deleteIcon,
-                    contentDescription = null,
-                    modifier = Modifier.padding(horizontal = 8.dp),
-                )
-            }
         }
+        Image(
+            painter = painterResource(id = R.drawable.ic_delete_button),
+            contentDescription = null,
+            modifier = Modifier.wrapContentWidth(Alignment.End)
+                .clickable { deleteAddress },
+            alignment = Alignment.TopEnd
+        )
     }
 }
